f2d830606bf3f2db5dc7c5474d5ad7601d8096d4
Jonathan Martinez <jonathan6md123@yahoo.com>
2021-09-10 18:48:38 -0400

    Vertex Atrributes and Layouts: Told OpenGL how the data is layed out in the buffer in order to interpret it as a Triangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -39,6 +39,8 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
     /*ENDOF: Vertex Buffers and Drawing a Triangle*/
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
