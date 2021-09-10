fd35f0aa9f5f6ee279b07e6ec3ce85b5f1bf4f23
Jonathan Martinez <jonathan6md123@yahoo.com>
2021-09-10 15:23:04 -0400

    Setup GLEW in order to allow the usage of modern OpenGL

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -19,6 +21,11 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Initialize GLEW*/
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -27,7 +34,7 @@ int main(void)
 
         /*Legacy OpenGL: To make a simple Triangle*/
         glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(-0.5f, -0.5f); 
         glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
         glEnd();
