dbf1633626b3edf633f8352e165bc65585e003d1
Jonathan Martinez <jonathan6md123@yahoo.com>
2021-09-13 23:01:13 -0400

    Uniforms in OpenGL: Defined a uniform variable in Basic.shader . Then in Application.cpp, I located that uniform within that file and manipulated it's r value in the while loop in order to simulate a color animation.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -130,6 +130,7 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
     /* Initialize GLEW*/
     if (glewInit() != GLEW_OK)
         std::cout << "Error" << std::endl;
@@ -167,11 +168,22 @@ int main(void)
     glUseProgram(shader);
     /*ENDOF:*/
 
+    int location = glGetUniformLocation(shader, "u_color");
+    float r = 0.0f;
+    float increment = 0.05f;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+        r += increment;
 
         GLCall( glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr) );
         /* Swap front and back buffers */
